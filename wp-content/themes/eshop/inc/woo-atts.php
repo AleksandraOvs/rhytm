8a73058f52841d84e1daa593534439a8629a8b0b
aleksandra <aleksandra.ovsia@gmail.com>
2026-04-14 13:24:02 +0300

    eds

--- wp-content/themes/eshop/inc/woo-atts.php
+++ wp-content/themes/eshop/inc/woo-atts.php
@@ -178,7 +178,7 @@ add_action('wp_footer', function () {
 
         .single_variation_wrap {
             display: flex;
-            align-items: flex-start;
+            align-items: center;
             gap: 40px;
         }
 
